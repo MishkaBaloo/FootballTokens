68d0d3920266bfc27525512330dc40985a4dda36
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-11-18 14:39:36 +0200

    Initial Commit

---
+++ FootballTokens/FootballTokensApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballTokensApp.swift
+//  FootballTokens
+//
+//  Created by Michael on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct FootballTokensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
